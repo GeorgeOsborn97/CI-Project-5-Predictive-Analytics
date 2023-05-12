2d215e6deba1dbc421d45664eed2d7c7d68284a6
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-05-12 12:44:18 +0000

    get all pages running in app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from app_pages.multi_page import MultiPage
 # load pages scripts
 from app_pages.summary_page import summary_body_page
 from app_pages.sales_price_study_page import sales_price_study_page_body
-#from app_pages.sales_price_predictor_page import sales_price_predictor_page_body
+from app_pages.sales_price_predictor_page import sales_price_predictor_page_body
 from app_pages.hypothesis_page import hypothesis_page_body
-#from app_pages.ml_predict_sales_price_page import ml_predict_sales_price_page_body
+from app_pages.ml_predict_sales_price_page import ml_predict_sales_price_page_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Housing Sales Price Price Predictor")
@@ -19,14 +19,14 @@ app.add_page(
 app.add_page(
     "Housing Sales Price Study", sales_price_study_page_body
     )
-#app.add_page(
-#    "Sales Price Predictor", sales_price_predictor_page_body
-#    )
+app.add_page(
+    "Sales Price Predictor", sales_price_predictor_page_body
+    )
 app.add_page(
     "Project Hypothesis and Validation", hypothesis_page_body
     )
-#app.add_page(
-#    "ML: Housing Sales Price Prediction", ml_predict_sales_price_page_body
-#    )
+app.add_page(
+    "ML: Housing Sales Price Prediction", ml_predict_sales_price_page_body
+    )
 
 app.run()
